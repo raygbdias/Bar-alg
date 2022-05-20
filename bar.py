64a96e6a17738189f1e240b81a6488bf71997b9f
raygbdias <raygabrielsantana@gmail.com>
2022-05-20 14:44:08 +0100

    fixed issue with DB

--- bar.py
+++ bar.py
@@ -1,12 +1,13 @@
 import json, random, string
 
 class Bar():
-    def __init__(self, content, phrase, quantity=None):
+    def __init__(self, content, phrase, id, quantity=None):
         with open('db\\db.json', 'r') as db:
             self.data = json.load(db)
         self.content = content
         self.phrase = phrase
         self.quantity = quantity
+        self.id = id
 
 
     def get_items(self):
@@ -27,10 +28,9 @@ class Bar():
                 choice = input(self.phrase)
                 choices.append(choice)
 
-            id = "".join(random.choices(string.ascii_letters + string.digits, k=4))
             dict = {}
-            dict[id] = {}
-            dict[id][self.content] = choices
+            dict[self.id] = {}
+            dict[self.id][self.content] = choices
             try:
                 with open('db\\user_db.json', 'r+') as user_db:
                     user_data = json.load(user_db)
@@ -50,20 +50,33 @@ class Bar():
                 if item == 'Name':
                     self.name = input(f'{i}-{item}: ')
                     self.cocktails_db[self.name] = {}
+                    self.cocktails_db[self.name]['Flavours'] = []
+                    self.cocktails_db[self.name]['Types'] = []
+                    self.cocktails_db[self.name]['Ingredients'] = []
+                    self.cocktails_db[self.name]['Glass'] = []
                 else:
                 #elif item == 'Flavour':
-                    self.cocktails_db[self.name]['Flavours'] = []
                     x = 1
-                    for flavour in self.data[item]:
-                        print(f'{x}-{flavour}', end=', ')
+                    print('\n')
+                    for sub_item in self.data[item]:
+                        print(f'{x}-{sub_item}', end=', ')
                         x += 1
-                    print()
                     quantity = input("How many you want to add? ")
                     for option in range(int(quantity)):
                         self.cocktails_db[self.name][item].append(input(f'{i}-{item}: '))
 
                         #user_flavour = input("Choose a flavour: ")
                         #self.cocktails_db[self.name]['Flavours'].append(user_flavour)
+                    try:
+                        with open('db\\user_db.json', 'r+') as user_db:
+                            user_data = json.load(user_db)
+                            user_data.update(self.cocktails_db)
+                            user_db.seek(0)
+                            json.dump(user_data, user_db, indent=4)
+                    except FileNotFoundError:
+                            with open('db\\user_db.json', 'w') as user_db:
+                                json.dump(self.cocktails_db, user_db, indent=2)
+
                 i += 1 
         else:
             print_db()
@@ -78,10 +91,11 @@ if option == '1':
     Bar("cocktails", "\nType the infos of your cocktails: ", None).get_items()
 
 elif option == '2':
+    id = "".join(random.choices(string.ascii_letters + string.digits, k=4))
     quantity = input("\nHow many flavours you want to add? ")
-    Bar('flavour', "\nWhat are the flavour you like on your cocktails? (Select a number) ", quantity).get_items()
+    Bar('flavours', "\nWhat are the flavour you like on your cocktails? (Select a number) ", id, quantity).get_items()
     quantity = input("\nHow many types you want to add? ")
-    Bar('types', "\nWhat types of cocktails do you enjoy? (Select a number) ", quantity).get_items()
+    Bar('types', "\nWhat types of cocktails do you enjoy? (Select a number) ", id, quantity).get_items()
 
 #print(list(data['Flavour']))      #convert dict to list to get items
 
